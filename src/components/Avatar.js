308d6d92342795028ad48e2f68f5562eb124fa87
Niki Tester <bloatedniknak@gmail.com>
2022-11-27 14:09:45 +0000

    feat: add navigation links for signed in user

---
+++ src/components/Avatar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '../styles/Avatar.module.css';
+
+const Avatar = ({ src, height = 45, text }) => {
+    return (
+        <span>
+            <img src={src} height={height} width={height} alt="avatar" className={styles.Avatar} />
+            {text}
+        </span>
+    );
+};
+
+export default Avatar;
