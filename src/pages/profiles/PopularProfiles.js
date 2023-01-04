e5d113ab760fa2581670665da0f9c29aecd40888
Niki Tester <bloatedniknak@gmail.com>
2023-01-04 16:12:04 +0000

    feat: add mobile most followed profiles view

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -5,7 +5,7 @@ import { axiosReq } from '../../api/axiosDefaults';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import Asset from '../../components/Asset';
 
-const PopularProfiles = () => {
+const PopularProfiles = ({ mobile }) => {
     const [profileData, setProfileData] = useState({
         popularProfiles: { results: [] },
         pageProfile: { results: [] }
@@ -27,21 +27,31 @@ const PopularProfiles = () => {
                 console.log(err);
             }
         };
-        
+
         handleMount();
     }, [currentUser]);
 
     return (
-        <Container className={appStyles.Content}>
-            <p>Most followed profiles</p>
+        <Container className={`${appStyles.Content} ${mobile && 'd-lg-none text-center mb-3'}`}>
+            <p>Most Followed Profiles</p>
             {popularProfiles.results.length ? (
-                popularProfiles.results.map(profile => (
-                    <p key={profile.id}>{profile.owner}</p>
-                ))
+                mobile ? (
+                    <div className="d-flex justify-content-around" >
+                        {
+                            popularProfiles.results.slice(0, 4).map(profile => (
+                                <p key={profile.id}>{profile.owner}</p>
+                            ))
+                        }
+                    </div>
+                ) : (
+                    popularProfiles.results.map(profile => (
+                        <p key={profile.id}>{profile.owner}</p>
+                    ))
+                )
             ) : (
                 <Asset spinner />
             )}
-        </Container>
+        </Container >
     );
 };
 
