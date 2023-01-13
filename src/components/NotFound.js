702eef4767350222eb345a00caa28716ae75be7c
Niki Tester <bloatedniknak@gmail.com>
2023-01-13 00:34:14 +0000

    feat: add NotFound.js component

---
+++ src/components/NotFound.js
@@ -0,0 +1,16 @@
+import NoResults from '../assets/no-results.png';
+import styles from '../styles/NotFound.module.css';
+import Asset from '../components/Asset';
+
+const NotFound = () => {
+	return (
+		<div className={styles.MarginTop}>
+			<Asset
+				src={NoResults}
+				message={"Sorry, the page you're looking for doesn't exist"}
+			/>
+		</div>
+	);
+};
+
+export default NotFound;
